c684dddfc0e2f9347582388f434bc4e76b85ff2c
Santiago Castro Herrera <159493032+Luismi0415@users.noreply.github.com>
2024-02-18 11:13:15 -0500

    Momento 1
    El código con su clases, atributos y métodos

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,37 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento asientos[];
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	public int cantidadAsientos(){
+		int i=0;
+		for(int j=0;j<asientos.length;j++) {
+			if(asientos[j]instanceof Asiento) {
+				i++;
+			}
+		}
+	return i;
+	}
+	public String verificarIntegridad() {
+		String prueba="Auto original";
+		for (int i= 0;i<asientos.length;i++) {
+			if(asientos[i]instanceof Asiento){
+				if((asientos[i].registro!=registro)||(registro!=motor.registro)) {
+					prueba="Las piezas no son originales";
+				}
+				
+			}
+				
+						
+			}
+		return prueba;
+		}
+	}
+
+
